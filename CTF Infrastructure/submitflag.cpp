abbc0f2a19e9616a0e7d520f90dd6552b30c8760
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2019-05-29 12:27:54 -0700

    Removed out of date / insecure infrastructure

--- CTF Infrastructure/submitflag.cpp
+++ CTF Infrastructure/submitflag.cpp
@@ -8,14 +8,14 @@ char socket_path[] = "/var/lib/flagserver/flagsocket";
 
 int main(int argc, char* argv[]) {
     if (argc == 1) {
-        printf("Usage: flagdump <flag>\n");
+        printf("Usage: submitflag <flag>\n");
         exit(0);
     }
 
     struct sockaddr_un svr_addr;
-    char flag_buf[21];
-    char usr_buf[33];
-    char send_buf[55] {0};
+    char flag_buf[51];
+    char usr_buf[51];
+    char send_buf[101] {0};
     int fd, svr_len, n;
 
     if( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
@@ -33,18 +33,24 @@ int main(int argc, char* argv[]) {
         exit(-1);
     }
 
-    getlogin_r(usr_buf, 32);
+    getlogin_r(usr_buf, 50);
     strncpy(flag_buf, argv[1], sizeof(flag_buf) - 1);
 
-    strncpy(send_buf, flag_buf, sizeof(flag_buf) - 1);
-    strncat(send_buf, " ", 1);
-    strncat(send_buf, usr_buf, sizeof(usr_buf) - 1);
-    strncat(send_buf, " ", 1);
+    for (int i = 0; i++; i < 50) {
+        if (flag_buf[i] == '\xff')
+            flag_buf[i] = '_';
+        if (usr_buf[i] == '\xff')
+            usr_buf[i] = '_';
+    }
 
+    strncpy(send_buf, usr_buf, sizeof(usr_buf) - 1);
+    strncat(send_buf, "\xff", 1);
+    strncat(send_buf, flag_buf, sizeof(flag_buf) - 1);
+    strncat(send_buf, "\xff", 1);
 
     write(fd, send_buf, strlen(send_buf));
     memset(&send_buf, 0, sizeof(send_buf));
-    n = read(fd, send_buf, 54);
+    n = read(fd, send_buf, 100);
     printf("%s", send_buf);
     close(fd);
 
